28adc7acba60dadbd801878c024bda9839cfb5de
Jessica Cooper <jessicagcooper@gmail.com>
2020-09-10 16:25:53 -0500

    Setting up EditBridge components.

---
+++ src/components/pages/EditBridge/EditBridgeForm.js
@@ -0,0 +1,27 @@
+// country
+// province
+// district
+// sector
+// cell
+// bridge_site_name
+// project_state
+// sub_stage
+// project_code
+// bridge_type
+// span
+// lat
+// long
+// individuals_directly_served
+// form_name
+// casesafeid_form
+// bridge_opportunity_id
+// bridge_image
+import React, { useState, useEffect, Fragment, useContext } from 'react';
+import { Link, withRouter } from 'react-router-dom';
+import { BridgesContext } from '../../../state/bridgesContext';
+
+const EditBridgeForm = ({ bridgeData }) => {
+  return <div></div>;
+};
+
+export default EditBridgeForm;
---
+++ src/components/pages/EditBridge/EditBridgeFormContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import EditBridgeForm from './EditBridgeForm';
+
+const EditBridgeFormContainer = () => {
+  return (
+    <div>
+      <EditBridgeForm />
+    </div>
+  );
+};
+
+export default EditBridgeFormContainer;
